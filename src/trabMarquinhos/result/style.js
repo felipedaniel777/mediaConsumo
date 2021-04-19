5080e12f52ea756f9954a96348f0248c3342e8c8
felipedaniel777 <felipedanielbecker@gmail.com>
2021-04-19 20:39:38 -0300

    gasosa

---
+++ src/trabMarquinhos/result/style.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+
+export default StyleSheet.create({
+  background: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#00d0ff",
+  },
+  boxResult: {
+    marginTop: "10%",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "80%",
+    height: "30%",
+    backgroundColor:'#00b3ff',
+    borderRadius:10,
+    borderWidth: 1,
+    borderColor: '#00b3ff'
+  },
+});
